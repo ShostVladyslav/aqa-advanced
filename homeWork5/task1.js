15b7d06f40cb584985b19f21ae376d1c70e5db21
Vlad <shostvladyslav@gmail.com>
2024-01-30 21:35:09 +0200

    add home Work #5

---
+++ homeWork5/task1.js
@@ -0,0 +1,22 @@
+//function declaration
+function calculateAreaRectangle1(width, height) {
+    const areaRectangle = width * height
+    return areaRectangle
+}
+
+console.log('function declaration area Rectangle:', calculateAreaRectangle1(5, 10))
+
+//function expression
+const calculateAreaRectangle2 = function (width, height) {
+    const areaRectangle = width * height
+    return areaRectangle
+}
+
+console.log('function expression area Rectangle:', calculateAreaRectangle2(5, 10))
+
+//arrow function expression
+const calculateAreaRectangle3 = (width, height) => {
+    const areaRectangle = width * height
+    return areaRectangle
+}
+console.log('arrow function expression area Rectangle:', calculateAreaRectangle3(5, 10))
