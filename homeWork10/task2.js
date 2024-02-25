297ef5d406a71ec323b65d0d9bc4b6bd6f3932e5
Vlad <shostvladyslav@gmail.com>
2024-02-25 20:09:57 +0200

    add eslint and prettier

--- homeWork10/task2.js
+++ homeWork10/task2.js
@@ -6,20 +6,20 @@ function fetchToDoById(id) {
 
 function fetchUsersById(id) {
     return fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-    .then((res) => res.json())
-    .then((json) => json)
+        .then((res) => res.json())
+        .then((json) => json)
 }
 
 Promise.all([
-    fetchToDoById(1), 
-    fetchUsersById(1)
+    fetchToDoById(1),
+    fetchUsersById(1),
 ])
     .then((values) => console.log('Results:', values))
     .catch((e) => console.error('Error:', e.message));
 
 Promise.race([
-    fetchToDoById(1), 
-    fetchUsersById(1)
+    fetchToDoById(1),
+    fetchUsersById(1),
 ])
     .then((values) => console.log('Results:', values))
     .catch((error) => console.error('Error:', error.message));
